d478d23a44e83652556e388efa911d74b8fd680b
Shunya Sato <shunya.s@jp.tdk.com>
2018-02-06 18:11:09 +0900

    simple shoot and upload

--- camcode.py
+++ camcode.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from time import sleep
+from datetime import datetime
+import os
 import base64
 from PIL import Image
 from Adafruit_IO import Client
@@ -10,18 +12,67 @@ from picamera import PiCamera
 import .secret
 
 aio = Client('SECRET')
+topic = 'picam1'
 BASE_DIR='/home/pi/Pictures/'
-DST_FILE=BASE_DIR+'snap.jpg'
 
 camera = PiCamera()
 camera.resolution = (320, 240)
-#camera.resolution = (1024, 768)
+# camera.resolution = (1024, 768)
+# camera.sharpness = 0
+# camera.contrast = 0
+# camera.brightness = 50
+# camera.saturation = 0
+# camera.ISO = 0
+# camera.video_stabilization = False
+# camera.exposure_compensation = 0
+# camera.exposure_mode = 'auto'
+# camera.meter_mode = 'average'
+# camera.awb_mode = 'auto'
+# camera.image_effect = 'none'
+# camera.color_effects = None
+# camera.rotation = 0
+# camera.hflip = False
+# camera.vflip = False
+# camera.crop = (0.0, 0.0, 1.0, 1.0)
+
 #camera.start_preview()
 # Camera warm-up time
 sleep(2)
-camera.capture(DST_FILE)
 
-with open(DST_FILE, "rb") as imageFile:
-    str = base64.b64encode(imageFile.read())
+def get_dirs(base_dir, d):
+    """
+    d: datetime.datetime instance
+    creates directries for
+    -- year
+     |- month
+      |- day
+    """
+    dirs = os.path.join(base_dir, str(d.year), str(d.month), str(d.day))
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+    return dirs
 
-aio.send('pic', str )
+def get_filename(d):
+    """
+    d: datime.datetime instance
+    reutnrs '20180205091142.jpg'
+    """
+    timestamp = d.strftime("%Y%m%d%H%M%S")
+    return "{}.jpg".format(timestamp)
+
+def shoot_post(aio, topic):
+    """
+    aio: Adafruit_IO.Client instance
+    topic: topic name to publish to
+    """
+    now = datetime.utcnow()
+    dirs = get_dirs(BASE_DIR, now)
+    pic_name = os.path.join(dirs, get_filename(now))
+    camera.capture(pic_name)
+
+    with open(pic_name, "rb") as imageFile:
+        img64 = base64.b64encode(imageFile.read())
+
+    aio.send(topic, img64 )
+
+shoot_post(aio, topic)
